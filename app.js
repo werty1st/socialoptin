2818aa5f5bb07ba86745788ca91da5517985d44c
werty1st <adams@werty1st.de>
2014-11-04 12:23:38 +0100

    final socket dropped

--- app.js
+++ app.js
@@ -4,15 +4,14 @@ var express = require('express');
 var app = express();
     app.enable('trust proxy');
     
-    app.use('/settings',express.static(__dirname+'/html'));
-    app.use('/css',express.static(__dirname+'/html/css'));
-    app.use('/js',express.static(__dirname+'/html/js'));
+    app.use('/settings/',express.static(__dirname+'/html'));
+    app.use('/css/',express.static(__dirname+'/html/css'));
+    app.use('/js/',express.static(__dirname+'/html/js'));
 
 var server = app.listen(process.env.PORT || 3002, function() {
     console.log('Listening on port %d', server.address().port);
 });
 
-var io = require('socket.io').listen(server); // this tells socket.io to use our express server
 
 // app.get('/settings', function(req, res){
 //     res.redirect(301, 'http://www.zdf.de/ZDFxt/module/socialoptin/settings/')
@@ -31,40 +30,6 @@ app.get('/', function(req, res){
     res.sendFile(path.join(__dirname, 'html', 'testjs.html'));
 
 });
-
-
-
-function socketfunction (socket) {
-
-
-    //vom client aufgerufen
-    socket.on('socket.save', function (data) {
-        console.log('socket.save',data);
-    });
-
-    socket.on('socket.load', function (data) {
-        console.log('socket.load',data);
-    });
-
-    // Success!  Now listen to messages to be received
-    socket.on('message',function(event){
-        console.log('Received message from client!',event);
-    });
-
-    socket.on('disconnect',function(){
-        console.log('Server has disconnected');
-    });
-
-    io.sockets.on('connection', function (socket) {
-        console.log('A new user connected!');
-    });
-
-};
-
-var ws1 = io.of('/');
-    ws1.on('connection', socketfunction);
-
-
 
 
 
